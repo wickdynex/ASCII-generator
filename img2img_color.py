3286226bc4afca5caeacf0d6d874adf72f684079
YiXuan Ding <1328032567@qq.com>
2024-11-22 14:46:09 +0800

    <add>: add `MIT License` and author info to each source code files.

--- img2img_color.py
+++ img2img_color.py
@@ -1,3 +1,16 @@
+# MIT License
+# 
+# Copyright (c) 2019 - 2024 Viet Nguyen <nhviet1009@gmail.com>
+# 
+# Permission is hereby granted, free of charge,
+# to any person obtaining a copy of this software and associated documentation files (the 'Software'),
+# to deal in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software
+# and to permit persons to whom the Software is furnished to do so
+# 
+# The above copyright notice
+# and this permission notice
+# shall be included in all copies or substantial portions of the Software.
 """
 @author: Viet Nguyen <nhviet1009@gmail.com>
 """
